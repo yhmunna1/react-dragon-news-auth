61add06eb839b9e1d30cfccc27826fe15ddaf32c
yhmunna1 <yh.munna.barisal@gmail.com>
2024-09-01 18:16:06 +0600

    layout for home page file created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Header from "../Shared/Header/Header";
+import Navbar from "../Shared/Navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
+      <Header></Header>
+      <Navbar></Navbar>
       <h2 className="text-3xl font-bold font-poppins">This is Home</h2>
     </div>
   );
